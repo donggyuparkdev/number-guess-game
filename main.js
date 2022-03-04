cb0683d35e774e797c6a0a423d9b02249957a9a5
DongGyu Park <donggyu.park.dev@gmail.com>
2022-03-04 23:28:06 +0900

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 let computerNum = 0;
-let playButton = document.getElementById("play-button")
+let playButton = document.getElementById("play-button");
+let userInput = document.getElementById("user-input");
+let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
 
 playButton.addEventListener("click",play);
 
@@ -11,15 +14,27 @@ function pickRandomNum() {
 }
 
 // 2. User 번호 입력 -> go버튼 누름
+// 3. 정답일시 -> 맞췄습니다.
+// 4. 랜덤번호 < User 번호 = Down!
+// 5. 랜덤번호 > User 번호 = Up!
 function play() {
-    console.log("게임시작");
+    let userValue = userInput.value;
+    if(userValue < computerNum) {
+        resultArea.textContent = "Up!!!"
+        // console.log("Up!!!")
+    } else if (userValue > computerNum) {
+        resultArea.textContent = "Down!!!"
+        // console.log("Down!!!")
+    } else {
+        resultArea.textContent = "맞췄습니다!!!"
+        // console.log("맞췄습니다!!!")
+    }
+
 }
 
 pickRandomNum();
 
-// 3. 정답일시 -> 맞췄습니다.
-// 5. 랜덤번호 > User 번호 = Up!
-// 4. 랜덤번호 < User 번호 = Down!
+
 // 6. Reset버튼 -> 게임리셋
 // 7. 5번 기회 다쓰면 게임 끝 (버튼, disable)
 // 8. 1~100 범위 밖 숫자 입력시 알람. 기회 깍지 않음
